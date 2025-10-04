a48a08ae577cf2411ed94a969ba1fa62e5154104
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-10-04 11:47:50 -0300

    Add moveToContactUsPage function for navigation

--- cypress/pages/utils/navigation.js
+++ cypress/pages/utils/navigation.js
@@ -4,6 +4,10 @@ export function moveToLoginPage() {
   cy.get('a[href="/login"]').click();
 }
 
+export function moveToContactUsPage() {
+  cy.get('a[href="/contact_us"]').click();
+}
+
 export function launchCommercePlatform() {
   cy.visit("https://www.automationexercise.com");
 }
