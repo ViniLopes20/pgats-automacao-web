9cc05651c5cdc296843a5ce45c91440e109d5920
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-10-31 01:16:59 -0300

    Add navigation function to move to the product page

--- cypress/pages/utils/navigation.js
+++ cypress/pages/utils/navigation.js
@@ -8,6 +8,10 @@ export function moveToContactUsPage() {
   cy.get('a[href="/contact_us"]').click();
 }
 
+export function moveToProductPage() {
+  cy.get('a[href="/products"]').click();
+}
+
 export function launchCommercePlatform() {
   cy.visit("https://www.automationexercise.com");
 }
