acc0b2f771932c17317ffad4b9f080d7b45de4a5
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-10-31 19:53:35 -0300

    Add addProductsToCart function to handle product addition to cart

--- cypress/pages/product/productPage.js
+++ cypress/pages/product/productPage.js
@@ -1,5 +1,17 @@
 /// <reference types="cypress"/>
 
+export function addProductsToCart(productIdList) {
+  productIdList.forEach((productId) => {
+    cy.get(`a[data-product-id="${productId}"]`).first().click();
+
+    cy.get("div.modal-content")
+      .should("be.visible")
+      .and("contain.text", "Your product has been added to cart.");
+
+    cy.get("button[data-dismiss='modal']").click();
+  });
+}
+
 export function searchForProduct(searchTerm) {
   cy.get("input#search_product").type(searchTerm);
   cy.get("button#submit_search").click();
