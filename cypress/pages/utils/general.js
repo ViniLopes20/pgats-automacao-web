b9e9545603875e415f8ad46419078ed3b0e94602
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-10-31 01:17:17 -0300

    Add scrollToElement function for improved element visibility in tests

--- cypress/pages/utils/general.js
+++ cypress/pages/utils/general.js
@@ -3,3 +3,7 @@
 export function assertUrlContainsPath(path) {
   cy.url().should("include", path);
 }
+
+export function scrollToElement(selectorToScroll) {
+  cy.get(selectorToScroll).scrollIntoView();
+}
